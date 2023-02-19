44d2b795e73774fdf8b8706cb60b4150b462be0c
Mohammed Almusaddar <mohammed@cloudfalcon.io>
2023-02-19 07:47:06 +0000

    add gitconfig

--- setup.sh
+++ setup.sh
@@ -13,3 +13,11 @@ done
 cd $CURRENT
 
 rm -rf $TMPDIR
+
+
+# aliases
+rm -f ~/.bash_aliases && cp ~/.dotfiles/.bash_aliases ~/.bash_aliases
+chmod +x ~/.bash_aliases
+
+# Git config
+sudo rm -f ~/.gitconfig && cp ~/.dotfiles/.gitconfig ~/.gitconfig
